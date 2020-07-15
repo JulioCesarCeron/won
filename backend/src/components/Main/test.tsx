7a0d11f8b89e6591873a64ab705ccbbbe14a97d5
Julio Ceron <julio.ceron@endurance.com>
2020-07-14 23:40:09 -0300

    adds eslint sintaxe rules

--- backend/src/components/Main/test.tsx
+++ backend/src/components/Main/test.tsx
@@ -2,14 +2,13 @@ import { render, screen } from '@testing-library/react';
 import Main from '.';
 
 describe('<Main />', () => {
+  it('should snapshot the heading', () => {
+    const { container } = render(<Main />);
 
-	it('should snapshot the heading', () => {
-		const { container } = render(<Main />);
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument();
 
-		expect(
-			screen.getByRole('heading', { name: /react avançado/i })
-		).toBeInTheDocument()
-
-		expect(container.firstChild).toMatchSnapshot()
-	})
-})
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
--- backend/src/pages/_document.tsx
+++ backend/src/pages/_document.tsx
@@ -1,19 +1,25 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,13 +28,13 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
-	}
+  }
 
-	render() {
+  render() {
     return (
       <Html lang="pt-BR">
         <Head />
@@ -37,6 +43,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
